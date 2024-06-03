585a12f9fb24a028f11dc3da85ea2aee8ac48ad8
cksghks89 <song960705@naver.com>
2024-06-03 16:20:00 +0900

    mysite3: MainController import 수정

--- mysite3/src/main/java/com/poscodx/mysite/controller/MainController.java
+++ mysite3/src/main/java/com/poscodx/mysite/controller/MainController.java
@@ -1,11 +1,7 @@
 package com.poscodx.mysite.controller;
 
-import java.util.Enumeration;
-
-import javax.servlet.ServletContext;
 import javax.servlet.http.HttpServletRequest;
 
-import org.apache.catalina.core.ApplicationContext;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
