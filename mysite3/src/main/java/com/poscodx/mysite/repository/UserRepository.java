ab5c5339ead5bac4f40ec1e679b79855c371d4a9
cksghks89 <song960705@naver.com>
2024-06-05 13:48:22 +0900

    mysite3: connection 정리

--- mysite3/src/main/java/com/poscodx/mysite/repository/UserRepository.java
+++ mysite3/src/main/java/com/poscodx/mysite/repository/UserRepository.java
@@ -1,16 +1,10 @@
 package com.poscodx.mysite.repository;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
 import java.util.Map;
 
 import org.apache.ibatis.session.SqlSession;
 import org.springframework.stereotype.Repository;
 
-import com.poscodx.mysite.exception.UserRepositoryException;
 import com.poscodx.mysite.vo.UserVo;
 
 @Repository
@@ -35,20 +29,5 @@ public class UserRepository {
 
 	public int update(UserVo vo) {
 		return sqlSession.update("user.update", vo);
-	}
-
-	private Connection getConnection() throws SQLException {
-		Connection conn = null;
-
-		try {
-			Class.forName("org.mariadb.jdbc.Driver");
-
-			String url = "jdbc:mariadb://192.168.64.3:3306/webdb?charset=utf8";
-			conn = DriverManager.getConnection(url, "webdb", "webdb");
-		} catch (ClassNotFoundException e) {
-			System.out.println("드라이버 로딩 실패:" + e);
-		}
-
-		return conn;
 	}
 }
